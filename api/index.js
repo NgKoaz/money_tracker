3fa3403ede369b819d240357b0babde1e90a02a3
NgKoaz <nguyenkhoa220703@gmail.com>
2023-08-25 18:28:21 +0700

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.post('/api/transaction', async (req, res) => {
     res.json(transaction)
 });
 
-const PORT = 4040;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 4040;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
